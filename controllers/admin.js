ec461e05790d33c6b9e96897b0b7ed4624f86a38
Nick Woodward <nick.woodward@hotmail.co.uk>
2021-06-08 03:09:05 +0100

    adding admin routes/logic

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const { validationResult } = require('express-validator');
 
 const Application = require('../models/application');
 const Applicant = require('../models/applicant');
@@ -8,12 +9,30 @@ const Person = require('../models/person');
 const Job = require('../models/job');
 const Company = require('../models/company');
 
+exports.deleteApplicant = (req, res, next) => {
+    Applicant.findOne({
+        where: { personId: req.params.id },
+        include: Person
+    }).then(applicant => {
+        if(!applicant) {
+            const error = new Error();
+            error.message = 'No User Found';
+            error.statusCode = 404;
+            next(error);
+        }
+        // Destroying the person cascades to the applicant and the applications
+        return applicant.person.destroy();
+    }).then(applicant => {
+        res.status(204).json({ msg: `User ${applicant.id} deleted` });
+    }).catch(err => {
+        console.log(err);        
+    }); 
+};
 
 exports.editApplicant = (req, res, next) =>{
-    // console.log(req.params)
-    console.log(req.body.firstName);
     
-    Applicant.findByPk(req.params.id, {
+    Applicant.findOne({
+        where: { personId: req.params.id },
         include: Person
     }).then(applicant => {
             if(!applicant) { 
@@ -27,7 +46,6 @@ exports.editApplicant = (req, res, next) =>{
             
             return applicant.save();
 
-            // return res.save();
         }).then(applicant => {
             const person = applicant.person;
 
@@ -142,5 +160,150 @@ exports.getCv = (req, res, next) => {
         });
 
    
+};
+
+exports.getJobs = (req, res, next) => {
+    Job.findAndCountAll({
+        attributes: [
+            'id',
+            'title',
+            'wage',
+            'location',
+            'description',
+            'featured',
+            [Sequelize.fn('date_format', Sequelize.col('job.createdAt' ), '%d/%m/%y'), 'createdAt'],
+            'companyId'
+        ],
+        distinct: true,
+        include: [ 
+            {
+                model: Company,
+                attributes: ['name', 'address']
+            }, 
+            {
+                model: Applicant,
+                attributes: [
+                    'id', 
+                    'cvUrl', 
+                    'personId',
+                    [Sequelize.fn('date_format', Sequelize.col('applicants.createdAt' ), '%d/%m/%y'), 'createdAt'],
+                ],
+                include: [ 
+                    {
+                        model: Person,
+                        attributes: [ 'firstName', 'lastName', 'phone', 'email' ]
+                    } 
+                ] 
+            }
+        ]
+    })
+    .then(jobs => {
+        jobs.rows = jobs.rows.map(({
+            id, title, wage, location, description, featured, createdAt: jobDate, companyId,
+            company: { name: companyName, address: companyAddress },
+            applicants,
+        }) => {
+            applicants = applicants.map(({ id, cvUrl, createdAt: appliedDate, personId, person: { firstName, lastName, phone, email } }) => {
+                const cvType = cvUrl? cvUrl.slice(cvUrl.lastIndexOf('.')):null;
+                const cvName = cvUrl? cvUrl.slice(12): 'No CV uploaded';
+
+                return { id, personId, firstName, lastName, cvType, cvName, appliedDate, phone, email };
+            });
+            
+            return { 
+                id,
+                title, 
+                wage, 
+                location, 
+                description, 
+                featured, 
+                jobDate,
+                companyId, 
+                companyName, 
+                companyAddress,
+                applicants
+             };
+        });
+
+        res.status(200).json({ msg: 'Success', jobs: jobs.rows, total: jobs.count });
+    })
+    .catch(err => {
+        throw err;
+    });
+};
+
+// @TODO: add validation
+exports.editJob = (req, res, next) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        const error = new Error();
+        error.message = 'Validation Error';
+        error.statusCode = 422;
+
+        throw (error);
+    }
+
+    Job.findOne({
+        where: { id: req.params.id },
+        include: Company 
+    })
+    .then(job => {
+        if(!job) {
+            return res.status(404).json({ message: 'Job not found' });
+        }
+
+        job.title = req.body.title;
+        job.wage = req.body.wage;
+        job.location = req.body.location;
+        job.description = req.body.description;
+        job.featured = req.body.featured;
+        
+        return job.save();
+    }).then(job => {
+        const company = job.company;
+        company.name = req.body.companyName;
+        console.log(company.name);
+        return company.save();
+    })
+    .then(company => {
+        res.status(200).json({ message: 'Job edited' });
+    })
+    .catch(err => console.log(err));
+};
+
+// @TODO: add validation
+exports.createJob = (req, res, next) => {
+    const errors = validationResults(req);
+    if(!errors.isEmpty()) {
+        const error = new Error('Validation Error');
+        error.statusCode = '422';
+        throw error;
+    }
+
+    Job.create({
+        title: req.body.title,
+        wage: req.body.wage,
+        location: req.body.location,
+        description: req.body.description,
+        feature: req.body.featured
+    }).then(job => {
+
+    }).catch(err => console.log(err));
+};
+
+exports.getCompanies = (req, res, next) => {
+    Company.findAll({
+        attributes: [ 
+            'id', 
+            'name',
+            [Sequelize.fn('date_format', Sequelize.col('company.createdAt' ), '%d/%m/%y'), 'createdAt']
+        ]
+    }).then(companies => {
+        res.status(200).json({ message: 'Success', companies });
+    }).catch(err => {
+        throw err;
+    });
 };
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const { uploadFile } = require('../middleware/multer');
+const multer = require('multer');
 
 const adminController = require('../controllers/admin');
 
 router.get('/applicants', adminController.getApplicants);
 router.get('/cvs/:applicantId', adminController.getCv);
+router.get('/jobs', adminController.getJobs);
+router.get('/companies', adminController.getCompanies);
 
-router.post('/applicants/:id', uploadFile('cv'), adminController.editApplicant)
+// @TODO: validation
+router.post('/edit/applicant/:id', uploadFile('cv'), adminController.editApplicant);
+// @TODO: add validation
+router.post('/edit/job/:id', multer().none(), adminController.editJob);
+// @TODO: add validation
+router.post('/new/job', multer().none(), adminController.createJob);
+
+router.delete('/delete/applicant/:id', adminController.deleteApplicant);
 
 module.exports = router;
